84d89150cde1a0242d8b954a1c4690c62d9c1db8
psguo <guopengsheng@gmail.com>
2018-05-01 17:16:44 -0400

    quaternion transform

--- project_double_grasp.py
+++ project_double_grasp.py
@@ -10,6 +10,7 @@ import scipy
 # OpenRAVE
 import openravepy
 from numpy import linalg as LA
+from pyquaternion import Quaternion
 
 # openravepy.RaveInitialize(True, openravepy.DebugLevel.Debug)
 
@@ -49,6 +50,7 @@ class RoboHandler:
         # order grasps based on your own scoring metric
         self.order_grasps()
 
+        self.is_test = True
 
         # order grasps with noise
         # self.order_grasps_noisy()
@@ -88,8 +90,8 @@ class RoboHandler:
     def problem_init(self):
         # self.target_kinbody = self.env.ReadKinBodyXMLFile('models/objects/domino.kinbody.xml')
         # self.target_kinbody = self.env.ReadKinBodyXMLFile('models/objects/wood_block.kinbody.xml')
-        self.target_kinbody = self.env.ReadKinBodyXMLFile('models/objects/cheezit.kinbody.xml')
-        # self.target_kinbody = self.env.ReadKinBodyURI('models/objects/champagne.iv')
+        # self.target_kinbody = self.env.ReadKinBodyXMLFile('models/objects/cheezit.kinbody.xml')
+        self.target_kinbody = self.env.ReadKinBodyURI('models/objects/champagne.iv')
         # self.target_kinbody = self.env.ReadKinBodyURI('models/objects/winegoblet.iv')
         # self.target_kinbody = self.env.ReadKinBodyURI('models/objects/black_plastic_mug.iv')
 
@@ -103,21 +105,21 @@ class RoboHandler:
         self.gmodel_left = openravepy.databases.grasping.GraspingModel(
             self.robot_left, self.target_kinbody)
 
-        # self.gmodel_right = openravepy.databases.grasping.GraspingModel(
-        #     self.robot_right, self.target_kinbody)
+        self.gmodel_right = openravepy.databases.grasping.GraspingModel(
+            self.robot_right, self.target_kinbody)
         # if you want to set options, e.g.  friction
         options = openravepy.options
         options.friction = 0.1
         if not self.gmodel_left.load():
             self.gmodel_left.autogenerate(options)
-        # if not self.gmodel_right.load():
-        #     self.gmodel_right.autogenerate(options)
+        if not self.gmodel_right.load():
+            self.gmodel_right.autogenerate(options)
 
         self.graspindices_left = self.gmodel_left.graspindices
         self.grasps_left = self.gmodel_left.grasps
 
-        # self.graspindices_right = self.gmodel_right.graspindices
-        # self.grasps_right = self.gmodel_right.grasps
+        self.graspindices_right = self.gmodel_right.graspindices
+        self.grasps_right = self.gmodel_right.grasps
 
     def filter_grasp(self, grasp, indices, gmodel):
 
@@ -171,14 +173,109 @@ class RoboHandler:
         # # self.robot.SetActiveDOFs(self.manip.GetGripperIndices(),
         # #                          self.robot.DOFAffine.X + self.robot.DOFAffine.Y + self.robot.DOFAffine.Z)
 
-    def get_grasp_relative_pose(self, grasp, graspindices):
+    def quaternion_from_matrix(self, matrix, isprecise=False):
+        """Return quaternion from rotation matrix.
+
+        If isprecise is True, the input matrix is assumed to be a precise rotation
+        matrix and a faster algorithm is used.
+
+        q = quaternion_from_matrix(np.identity(4), True)
+        np.allclose(q, [1, 0, 0, 0])
+        True
+        q = quaternion_from_matrix(np.diag([1, -1, -1, 1]))
+        np.allclose(q, [0, 1, 0, 0]) or np.allclose(q, [0, -1, 0, 0])
+        True
+        R = rotation_matrix(0.123, (1, 2, 3))
+        q = quaternion_from_matrix(R, True)
+        np.allclose(q, [0.9981095, 0.0164262, 0.0328524, 0.0492786])
+        True
+        R = [[-0.545, 0.797, 0.260, 0], [0.733, 0.603, -0.313, 0],
+        ...      [-0.407, 0.021, -0.913, 0], [0, 0, 0, 1]]
+        q = quaternion_from_matrix(R)
+        np.allclose(q, [0.19069, 0.43736, 0.87485, -0.083611])
+        True
+        R = [[0.395, 0.362, 0.843, 0], [-0.626, 0.796, -0.056, 0],
+        ...      [-0.677, -0.498, 0.529, 0], [0, 0, 0, 1]]
+        q = quaternion_from_matrix(R)
+        np.allclose(q, [0.82336615, -0.13610694, 0.46344705, -0.29792603])
+        True
+        R = random_rotation_matrix()
+        q = quaternion_from_matrix(R)
+        is_same_transform(R, quaternion_matrix(q))
+        True
+        is_same_quaternion(quaternion_from_matrix(R, isprecise=False),
+        ...                    quaternion_from_matrix(R, isprecise=True))
+        True
+        R = euler_matrix(0.0, 0.0, np.pi/2.0)
+        is_same_quaternion(quaternion_from_matrix(R, isprecise=False),
+        ...                    quaternion_from_matrix(R, isprecise=True))
+        True
+
+        """
+        M = np.array(matrix, dtype=np.float64, copy=False)[:4, :4]
+        if isprecise:
+            q = np.empty((4,))
+            t = np.trace(M)
+            if t > M[3, 3]:
+                q[0] = t
+                q[3] = M[1, 0] - M[0, 1]
+                q[2] = M[0, 2] - M[2, 0]
+                q[1] = M[2, 1] - M[1, 2]
+            else:
+                i, j, k = 0, 1, 2
+                if M[1, 1] > M[0, 0]:
+                    i, j, k = 1, 2, 0
+                if M[2, 2] > M[i, i]:
+                    i, j, k = 2, 0, 1
+                t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+                q[i] = t
+                q[j] = M[i, j] + M[j, i]
+                q[k] = M[k, i] + M[i, k]
+                q[3] = M[k, j] - M[j, k]
+                q = q[[3, 0, 1, 2]]
+            q *= 0.5 / math.sqrt(t * M[3, 3])
+        else:
+            m00 = M[0, 0]
+            m01 = M[0, 1]
+            m02 = M[0, 2]
+            m10 = M[1, 0]
+            m11 = M[1, 1]
+            m12 = M[1, 2]
+            m20 = M[2, 0]
+            m21 = M[2, 1]
+            m22 = M[2, 2]
+            # symmetric matrix K
+            K = np.array([[m00 - m11 - m22, 0.0, 0.0, 0.0],
+                          [m01 + m10, m11 - m00 - m22, 0.0, 0.0],
+                          [m02 + m20, m12 + m21, m22 - m00 - m11, 0.0],
+                          [m21 - m12, m02 - m20, m10 - m01, m00 + m11 + m22]])
+            K /= 3.0
+            # quaternion is eigenvector of K that corresponds to largest eigenvalue
+            w, V = np.linalg.eigh(K)
+            q = V[[3, 0, 1, 2], np.argmax(w)]
+        if q[0] < 0.0:
+            np.negative(q, q)
+        return q
+
+    def get_grasp_relative_pose(self, grasp, gmodel, graspindices):
         direction=grasp[graspindices.get('igraspdir')]
         roll=grasp[graspindices.get('igrasproll')]
         position=grasp[graspindices.get('igrasppos')]
         standoff=grasp[graspindices.get('igraspstandoff')]
         manipulatordirection=grasp[graspindices.get('imanipulatordirection')]
 
-        pose = [position, direction]
+        # pose = [position, direction]
+        # gmodel.moveToPreshape(grasp)
+
+
+        Tgrasp = gmodel.GetLocalGraspTransform(grasp, collisionfree=True)
+        quaternion = self.quaternion_from_matrix(Tgrasp)
+        # basemanip = openravepy.interfaces.BaseManipulation(self.robot_left)
+        # basemanip.MoveToHandPosition(matrices=[Tgrasp+
+
+        # print("test")
+        position = Tgrasp[:3, 3]
+        return (position, quaternion)
 
         # order the grasps - call eval grasp on each, set the 'performance' index,
     # and sort
@@ -192,8 +289,12 @@ class RoboHandler:
             grasp_left = self.grasps_left[index_left]
             grasp_right = self.grasps_right[index_right]
 
-            grasp_left_relative_pose = self.get_grasp_relative_pose(grasp_left, self.graspindices_left)
-            grasp_right_relative_pose = self.get_grasp_relative_pose(grasp_right, self.graspindices_right)
+            grasp_left[self.graspindices_left.get('igrasppos')] = np.array([0,0,0])
+
+            # self.gmodel_left.showgrasp(grasp_left, showfinal=True)
+
+            grasp_left_relative_pose = self.get_grasp_relative_pose(grasp_left, self.gmodel_left, self.graspindices_left)
+            grasp_right_relative_pose = self.get_grasp_relative_pose(grasp_right, self.gmodel_right, self.graspindices_right)
 
             # Tgrasp_left = self.gmodel_left.getGlobalGraspTransform(grasp_left, collisionfree=True)
             # contacts, finalconfig, mindist, volume = self.gmodel_left.testGrasp(grasp=grasp_left, translate=True,
